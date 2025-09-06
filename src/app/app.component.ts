2f07f0e2f3a2f95076960551808cf630449d2a49
FloresAlvaro <alvarofloresfarfan2802@gmail.com>
2025-09-05 20:52:19 -0400

    feat: restructure app component and add topbar
    - Created app.component.html and moved existing HTML content from app.html.
    - Updated app.component.ts to reference the new HTML and SCSS files.
    - Added TopbarComponent with basic template and styles.
    - Removed app.html as it is no longer needed.
    - Updated index.html title from "1FrontendReportCard" to "Centralizador".
    - Added global styles in styles.scss, including Google Fonts and font weight classes.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TopbarComponent } from "./common/topbar/topbar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.scss',
+  imports: [RouterOutlet, TopbarComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
 })
+
 export class AppComponent {
   protected readonly title = signal('1.frontend_report_card');
 }
